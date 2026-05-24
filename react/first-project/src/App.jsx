eadd61492079d0afc30566c0f69c273adb4db7c7
shristitechacademy <sripriyamathan@gmail.com>
2026-05-24 11:52:36 +0530

    todo app

--- react/first-project/src/App.jsx
+++ react/first-project/src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { Courses } from "./Courses";
+import Greeter from "./Greeter";
 import { Header } from "./Header";
 // root component
 function App() {
+  const [course, setCourse] = useState("Cloud");
   return (
     <>
       <Header cname="Shristi Tech Academy"></Header>
-      <select name="search">
+      <Greeter uname="Sripriya"></Greeter>
+      <select name="search" onChange={(e) => setCourse(e.target.value)}>
         <option value="backend">Backend</option>
         <option value="frontend">Frontend</option>
       </select>
-      <Courses ctype="backend"></Courses>
+      <h2>The selected course is {course}</h2>
+      <Courses ctype={course}></Courses>
     </>
   );
 }
---
+++ react/first-project/src/Greeter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Greeter = ({uname}) => {
+    const[username,setUsername] = useState(uname)
+    const showMsg =()=>{
+        alert('Great day');
+        console.log('Have  a great day');
+    }
+    return (
+    <div>
+      <h1>Welcome {username}</h1>
+      <input type="text" name="choice" 
+             value={username} 
+             onChange={(e)=>setUsername(e.target.value)}></input>
+      <button onClick={()=>showMsg()}> ShowMessage</button>
+    </div>
+  )
+}
+
+export default Greeter
